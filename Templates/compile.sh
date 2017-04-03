d5423d200288ea3ce625bac8c08c039b1a209572
Cubitect <cubitect@gmail.com>
2017-04-04 00:38:27 +0100

    added ASMStructMarker

--- Templates/compile.sh
+++ Templates/compile.sh
@@ -2,10 +2,13 @@
 find . -name "*.class" -type f -delete
 jcmp='javac -cp .:../libs/lwjgl-2.9.3.jar'
 dasm='python2 ../Krakatau-master/disassemble.py'
-$jcmp asmtick/*.java villagemarker/*.java eventmarker/*.java
+$jcmp asmtick/*.java villagemarker/*.java eventmarker/*.java structmarker/*.java
+$dasm './asmrender/ASMRender.class'
 $dasm './asmtick/ASMTick.class'
 $dasm './asmtick/CommandTickrate.class'
 $dasm './villagemarker/ASMVillageMarker.class'
 $dasm './eventmarker/ASMEventMarker.class'
 $dasm './eventmarker/ASMEventMarker$TickEntry.class'
-$dasm './asmrender/ASMRender.class'
+$dasm './structmarker/ASMStructMarker.class'
+$dasm './structmarker/ASMStructMarker$BB.class'
+$dasm './structmarker/ASMStructMarker$StructFType.class'
